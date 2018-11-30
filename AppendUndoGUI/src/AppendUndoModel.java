6f8b18e6b200f21fe4bfa53083b923bafe6b7e9a
ShepDogSSB <nickshepard288150@gmail.com>
2018-11-30 10:49:38 -0500

    Initial Commit

---
+++ AppendUndoGUI/src/AppendUndoModel.java
@@ -0,0 +1,66 @@
+/**
+ * Model interface.
+ *
+ * The AppendUndo model consists of the output string and a stack of the strings
+ * appended to the output in reverse order.
+ *
+ * @author Bruce W. Weide
+ * @author Paolo Bucci
+ *
+ * @mathmodel <pre>
+ * {@code type AppendUndoModel is modeled by
+ *   (output: string of character,
+ *    output: string of string of character)}
+ * </pre>
+ * @initially <pre>
+ * {@code default:
+ *   ensures
+ *     this = ("", <>)}
+ * </pre>
+ */
+public interface AppendUndoModel {
+
+    /**
+     * Reports this.input.
+     *
+     * @return this.input
+     * @ensures <pre>
+     * {@code input = this.input}
+     * </pre>
+     */
+    String input();
+
+    /**
+     * Sets this.input to argument.
+     *
+     * @param input
+     *            new this.input value
+     * @ensures <pre>
+     * {@code this.input = input}
+     * </pre>
+     */
+    void setInput(String input);
+
+    /**
+     * Reports this.output.
+     *
+     * @return this.output
+     * @aliases reference returned by {@code output}
+     * @ensures <pre>
+     * {@code output = this.output}
+     * </pre>
+     */
+    String output();
+
+    /**
+     * Sets this.output to argument.
+     *
+     * @param output
+     *            new this.output value
+     * @ensures <pre>
+     * {@code this.output = output}
+     * </pre>
+     */
+    void setOutput(String output);
+
+}
